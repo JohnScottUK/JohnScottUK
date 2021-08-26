b5d45119bb00dbb4a4304270927fc1dc045ad4cc
John Scott <68079471+JohnScottUK@users.noreply.github.com>
2021-08-26 23:17:22 +0100

    Initial profile.

---
+++ README.md
@@ -0,0 +1,16 @@
+<style>
+table td, table td * {
+    vertical-align: top;
+}
+</style>
+<table><thead><tr><th valign="top" width="50%">About Me</th><th valign="top" width="50%">Blog</th>
+</thead><tbody><tr>
+<td>
+<p>I currently work for a bank in the UK, specifically working in the area of APIs and integration. I use a pragmatic blend of APIs, EDA, ESBs, REST, SOA, Web Services and more to connect and integrate systems. The contents of this site are entirely my own and do not represent the views or opinions of my employer.</p>
+
+<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="662bfb0a-faef-4686-a2ab-14abf55b4996" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+
+</td>
+<td><!-- begin blog -->
+<!-- end blog -->
+</td></tr></tbody></table>
---
+++ update_readme.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import feedparser
+from datetime import datetime as dt
+import os
+import re
+
+print("{}: Update README.md".format(os.path.basename(__file__)))
+
+url="https://www.jsware.io/feed.xml"
+filename = "{}/README.md".format(os.path.dirname(os.path.abspath(__file__)))
+
+print("Grabbing feed '{}'...".format(url))
+feed = feedparser.parse(url)
+entries = [
+  {
+    "title": entry["title"],
+    "link": entry["link"].split("#")[0],
+    "published": dt.strptime(entry["published"],"%Y-%m-%dT%H:%M:%S%z")
+  }
+  for entry in feed["entries"]
+]
+
+print("Generating markdown...")
+markdown = "\n".join(["* [{0}]({1})<sup>{2}</sup>".format(entry["title"], entry["link"], entry["published"].strftime("%d %b %Y")) for entry in entries])
+
+print("Reading '{}'...".format(filename))
+with open(filename) as f:
+  readme = f.read()
+
+readme = re.sub("<\!-- begin blog -->(\n|.)*<\!-- end blog -->", "<!-- begin blog -->\n\n" + markdown + "\n<!-- end blog -->", readme)
+
+print("Writing '{}'...".format(filename))
+with open(filename, "w") as f:
+  f.write(readme)
